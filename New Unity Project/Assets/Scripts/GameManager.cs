fc1eabd3250730ec0ed71c0db6feea06f5be7732
LORDS\Benjamin <bchouteau1@yahoo.com>
2018-02-25 15:55:31 -0700

    MileStone 1

---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+
+	void Awake () {
+
+		//Makes the game manager a singleton object
+		if (gm == null) {
+			gm = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+	}
+
---
+++ New Unity Project/Assets/Scripts/General_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class General_Movement : MonoBehaviour {
+
+    private Transform tf;
+    private Vector3 warpPos;
+
+    public float rotationSpeed;
+    public float linearSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            tf.Rotate(0, 0, 1 * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            tf.Rotate(0, 0, -1 * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            tf.position += tf.up * linearSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            tf.position -= tf.up * linearSpeed;
+        }
+    }
+}
